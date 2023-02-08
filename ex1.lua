5e4a29fa5df127177b35dcaf87dc126b3696f56d
Tovy <tovy.ren@epitech.eu>
2023-02-08 14:19:29 +0100

    [PRESENCE]: Tovy REN

---
+++ ex1.lua
@@ -0,0 +1,9 @@
+for i = 1, 100 do
+    if i % 15 == 0 then
+        print("ThreeFive")
+    elseif i % 3 == 0 then
+        print("Three")
+    elseif i % 5 == 0 then
+        print("Five")
+    end
+end
